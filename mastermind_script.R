8604b0845c30f794c41f8543bdbd048c7f19a38d
samsiljee <samsiljee@gmail.com>
2023-02-09 09:06:36 +1300

    Minor changes, mostly in layout and some changes to the graphics

--- mastermind_script.R
+++ mastermind_script.R
@@ -1,33 +1,34 @@
-mastermind <- function(n = 6, x = FALSE)
-{
-    # A code written by Sam Siljee based on the game Mastermind
-    # 2nd December 2020
-    
-    # Things to be improved still:
-    # 1. scoring not yet entirely compatible with some combinations of repeating colours
-    # 2. give an option to change the number of holes
-    
-    # load appropriate packages
-    library(tidyverse)
-    
-    # define the colours
+# A code written by Sam Siljee based on the game Mastermind
+# 2nd December 2020
+
+# Things to be improved still:
+# 1. scoring not yet entirely compatible with some combinations of repeating colours
+# 2. give an option to change the number of holes
+
+# load appropriate packages
+library(tidyverse)
+
+# Write the function
+mastermind <- function(n = 6, x = FALSE){
+    # define the colours
     colours <- c("yellow", "red", "blue", "purple", "green", "orange", "black", "white", "brown", "grey", "pink")
     
-    # create a named colours vector for grapihcal board
+    # create a named colours vector for graphical board
     namedcolours <- setNames(colours, colours)
     
     # create playing board
-    r = 0
-    board <- data.frame(first = NA, second = NA, third = NA, fourth = NA, B = NA, W = NA, R = r)
+    round = 0
+    board <- data.frame(first = NA, second = NA, third = NA, fourth = NA, black = NA, white = NA, round = round)
     ans = NA
     
     # set the solution
     solution <- paste(sample(colours[1:n], 4, replace = x))
-    print("Welcome to this game of Mastermind, enter quit to exit the game or show to reveal the answer. The number of colours can be chosen with a numerical value for the first argument when starting mastermind, maximum of eleven. Repetition of colours can be chosen as an option by entering TRUE as the second argument. default is FALSE.")
+    cat("Welcome to this game of Mastermind, enter quit to exit the game or show to reveal the answer.\n
+          The number of colours can be chosen with a numerical value for the first argument when starting mastermind, maximum of eleven.\n
+          Repetition of colours can be chosen as an option by entering TRUE as the second argument. default is FALSE.\n")
     
     # show the colour options
-    print("These are the available colours. Note that spelling has to be exact.")
-    print(colours[1:n])
+    cat("These are the available colours:", colours[1:n], "\b.\nNote that the spelling has to be exact.\n")
     
     # start while loop for incorrect answer
     while(!identical(ans, solution))
@@ -36,12 +37,12 @@ mastermind <- function(n = 6, x = FALSE)
         ans <- scan(what = character(), quiet = TRUE, nlines = 1)
         
         # provision to quit if quit entered
-        if(ans[1] %in% "quit"){
+        if(ans[1] %in% "quit" | ans[1] %in% "Quit"){
             break
         }
         
         # provision to show answer
-        if(ans[1] %in% "show"){
+        if(ans[1] %in% "show" | ans[1] %in% "Show"){
             print(solution)
         }
         
@@ -51,33 +52,35 @@ mastermind <- function(n = 6, x = FALSE)
         # check for number of blacks
         black <- sum(solution == ans)
         
-        # check for number of whites
+        # calculate number of whites
         white <- total_correct - black
         
         # add to round counter
-        r <- r + 1
+        round <- round + 1
         
         # create vector of latest guess with scoring
-        guess_vector <- c(ans, black, white, r)
+        guess_vector <- c(ans, black, white, round)
         
         # add vector to current board
         board <- rbind(board, guess_vector)
         
         # print current board (table format)
-        print(board[- 1, ])
+        print(board[-1, ])
         
         # print current board (graphic format)
-        graphic_board <- board[- 1, ] %>% ggplot() + 
+        graphic_board <- board[-1, ] %>% ggplot() + 
             scale_color_manual(values = namedcolours) + 
-            geom_point(aes(x = 1, y = R, colour = first), size = 10) + 
-            geom_point(aes(x = 2, y = R, colour = second), size = 10) + 
-            geom_point(aes(x = 3, y = R, colour = third), size = 10) + 
-            geom_point(aes(x = 4, y = R, colour = fourth), size = 10) +
-            theme(legend.position = "none")
+            geom_point(aes(x = 1, y = round, colour = first), size = 10) + 
+            geom_point(aes(x = 2, y = round, colour = second), size = 10) + 
+            geom_point(aes(x = 3, y = round, colour = third), size = 10) + 
+            geom_point(aes(x = 4, y = round, colour = fourth), size = 10) +
+            theme(legend.position = "none") +
+            xlab(NA)
         print(graphic_board)
         
     }
-    # print statement if solution correct
-    print("Congratulations, you have guessed the code!")
-    
+    # print the exit statement
+    ifelse(ans == solution, cat("\nCongratulations, you have guessed the code!"), cat("\nGame quit"))
+
 }
+
