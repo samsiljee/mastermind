b899b2d57d0e002b718f960ad8cec7a696b89536
samsiljee <samsiljee@gmail.com>
2023-02-09 14:31:34 +1300

    More code to add clue pins

--- mastermind_script.R
+++ mastermind_script.R
@@ -26,18 +26,14 @@ mastermind <- function(n = 6, x = FALSE){
                       black = NA,
                       white = NA,
                       round = round,
-                      clue_1_x = NA,
-                      clue_2_x = NA,
-                      clue_3_x = NA,
-                      clue_4_x = NA,
-                      clue_1_black_alpha = NA,
-                      clue_2_black_alpha = NA,
-                      clue_3_black_alpha = NA,
-                      clue_4_black_alpha = NA,
-                      clue_1_white_alpha = NA,
-                      clue_2_white_alpha = NA,
-                      clue_3_white_alpha = NA,
-                      clue_4_white_alpha = NA)
+                      clue_1_black_size = NA,
+                      clue_2_black_size = NA,
+                      clue_3_black_size = NA,
+                      clue_4_black_size = NA,
+                      clue_1_white_size = NA,
+                      clue_2_white_size = NA,
+                      clue_3_white_size = NA,
+                      clue_4_white_size = NA)
   ans = NA
   # set the solution
   solution <- paste(sample(colours[1:n], 4, replace = x))
@@ -73,17 +69,17 @@ mastermind <- function(n = 6, x = FALSE){
       # calculate number of whites
       white <- total_correct - black
       
-      # create alphas for the black clues
-      black_alphas <- c(rep(1, black), rep(0, 4 - black))
+      # create sizes for the black clues
+      black_sizes <- c(rep(5, black), rep(0, 4 - black))
       
-      # create alphas for the white clues
-      white_alphas <- c(rep(0, black), rep(1, white), rep(0, 4 - total_correct))
+      # create sizes for the white clues
+      white_sizes <- c(rep(0, black), rep(5, white), rep(0, 4 - total_correct))
       
       # add to round counter
       round <- round + 1
       
       # create vector of latest guess with scoring
-      guess_vector <- c(ans, black, white, round, seq(4.7, 5.3, length.out = 4), black_alphas, white_alphas)
+      guess_vector <- c(ans, black, white, round, black_sizes, white_sizes)
       
       # add vector to current board and convert to numeric where appropriate
       board <- rbind(board, guess_vector)
@@ -94,14 +90,18 @@ mastermind <- function(n = 6, x = FALSE){
       # print current board (graphic format)
       graphic_board <- board[-1, ] %>% ggplot() +
           scale_color_manual(values = namedcolours) +
-          geom_point(aes(x = 1, y = round, colour = first), size = 10) +
-          geom_point(aes(x = 2, y = round, colour = second), size = 10) +
-          geom_point(aes(x = 3, y = round, colour = third), size = 10) +
-          geom_point(aes(x = 4, y = round, colour = fourth), size = 10) +
-          geom_point(aes(x = clue_1_x, y = round), colour = "black", size = 5) +
-          geom_point(aes(x = clue_2_x, y = round), colour = "black", size = 5) +
-          geom_point(aes(x = clue_3_x, y = round), colour = "black", size = 5) +
-          geom_point(aes(x = clue_4_x, y = round), colour = "black", size = 5) +
+          geom_point(aes(x = 1, y = round, colour = first), size = 15) +
+          geom_point(aes(x = 2, y = round, colour = second), size = 15) +
+          geom_point(aes(x = 3, y = round, colour = third), size = 15) +
+          geom_point(aes(x = 4, y = round, colour = fourth), size = 15) +
+          geom_point(aes(x = 4.7, y = round, size = clue_1_black_size), colour = "black", alpha = 0.5) +
+          geom_point(aes(x = 4.9, y = round, size = clue_2_black_size), colour = "black", alpha = 0.5) +
+          geom_point(aes(x = 5.1, y = round, size = clue_3_black_size), colour = "black", alpha = 0.5) +
+          geom_point(aes(x = 5.3, y = round, size = clue_4_black_size), colour = "black", alpha = 0.5) +
+          geom_point(aes(x = 4.7, y = round, size = clue_1_white_size), colour = "white", alpha = 0.5) +
+          geom_point(aes(x = 4.9, y = round, size = clue_2_white_size), colour = "white", alpha = 0.5) +
+          geom_point(aes(x = 5.1, y = round, size = clue_3_white_size), colour = "white", alpha = 0.5) +
+          geom_point(aes(x = 5.3, y = round, size = clue_4_white_size), colour = "white", alpha = 0.5) +
           xlim(0.7, 5.5) +
           xlab("Position") +
           theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), legend.position = "none")
@@ -111,7 +111,7 @@ mastermind <- function(n = 6, x = FALSE){
       }
   
   # print the exit statement
-  ifelse(ans == solution, cat("\nCongratulations, you have guessed the code!"), cat("\nGame quit"))
+  ifelse(ans == solution, print("\nCongratulations, you have guessed the code!"), print("\nGame quit"))
 
 }
 
